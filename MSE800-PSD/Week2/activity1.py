72eb0bd7e707b4b229ff8494686d55256e9e67cb
M Kang <minqu.kang@gmail.com>
2026-04-18 09:52:58 +1200

    Update activity1.py

--- MSE800-PSD/Week2/activity1.py
+++ MSE800-PSD/Week2/activity1.py
@@ -26,9 +26,18 @@ def calc_complex():
     z = x + y
     return f'{x} + {y} = {z}, real = {z.real}, imag = {z.imag}'
 
+def factorial(n):
+    if n < 0:
+        return "Error: Factorial is not defined for negative numbers."
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
 print('add', add(10, 5))
 print('subtract', subtract(10, 5))
 print('multiply', multiply(2, 7))
 print('divide', divide(10, 4))
 print('modulo', modulo(11, 3))
 print('calc_complex', calc_complex())
+print('factorial', factorial(5))
