bb4e426e481ed93ce7649e0b36ade1232a41ec89
M Kang <minqu.kang@gmail.com>
2026-04-18 09:45:20 +1200

    Update activity1.py

--- MSE800-PSD/Week2/activity1.py
+++ MSE800-PSD/Week2/activity1.py
@@ -20,8 +20,15 @@ def modulo(x, y):
     else:
         return "Error: Division by zero is not allowed."
 
+def calc_complex():
+    x = 1 + 2j
+    y = 3 + 4j
+    z = x + y
+    return '{x} + {y} = {z}, real', z.real, 'imag = ', z.imag
+
 print('add', add(10, 5))
 print('subtract', subtract(10, 5))
 print('multiply', multiply(2, 7))
 print('divide', divide(10, 4))
 print('modulo', modulo(11, 3))
+print('calc_complex', calc_complex())
